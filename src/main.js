1d14e5d195e452d5e03c3aed66404ecdf62f81b5
rikyriyanto <riyanriky@gmail.com>
2026-02-21 11:14:09 +0700

    add pwa, add paste paste image feature

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import './style.css'
 import 'flowbite'
 import App from './App.vue'
 import router from './router'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 createApp(App).use(router).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,38 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-//   server: {
-//     host: "0.0.0.0",
-//     allowedHosts: ["beloved-noticeably-blowfish.ngrok-free.app"],
-//   },
-// })
+export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: ["beloved-noticeably-blowfish.ngrok-free.app"],
+  },
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg', 'menu_book.svg', 'pwa-192x192.svg', 'pwa-512x512.svg'],
+      manifest: {
+        name: 'Bacokitab',
+        short_name: 'Bacokitab',
+        description: 'Bantu bedah tata bahasa Arab',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'pwa-512x512.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5000000 
+      }
+    })
+  ]
+})
